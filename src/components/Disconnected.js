f55e63f11cac4d982d6645b786e1b3a93061baf3
susmit <susmit9370@gmail.com>
2020-08-09 11:42:48 +0530

    ratings via eth,metamask

--- src/components/Disconnected.js
+++ src/components/Disconnected.js
@@ -2,8 +2,27 @@ import React from 'react'
 import './Component.css'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+import { CADBURY_ABI, CADBURY_ADDRESS } from '../config'
+
+const Web3 = require("web3")
+
+const labels = {
+  1: 'Fine',
+  2: 'Ok',
+  3: 'Good',
+  4: 'Awesome',
+  5: 'Excellent',
+};
+
 
 const useStyles = makeStyles((theme) => ({
+  ratings: {
+    width: 200,
+    display: 'flex',
+    alignItems: 'center',
+  },
   root: {
     flexGrow: 1,
   },
@@ -39,8 +58,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+async function yoyo(val){
+            try{window.ethereum.enable();
+            const web3 = new Web3(Web3.givenProvider)
+            const accounts = await web3.eth.getAccounts()
+            console.log(accounts[0])
+            const CadburySmc = new web3.eth.Contract( CADBURY_ABI, CADBURY_ADDRESS)
+            console.log(CadburySmc.options.address)
+            const total = await CadburySmc.methods.totalPeople().call()
+            console.log(total)
+            const ratings = await CadburySmc.methods.GetRatings().call()
+            console.log(ratings)
+            const rating = await CadburySmc.methods.rate(val).send({ from: accounts[0] })
+            console.log(rating)
+          }catch(e){}
+}
+
 function Disconnected(props) {
   let classes = useStyles()
+  const [value, setValue] = React.useState(5);
+  const [hover, setHover] = React.useState(-1);
   return (
     <div className="App">
       <header className="App-header">
@@ -51,7 +88,33 @@ function Disconnected(props) {
             </Typography>
           </div>
           <h2>Disengaged</h2>
-          Hope you have a good day !
+          <p>How was the meeting ? (kovan) </p>
+          <br/>
+          <br/>
+          <div className={classes.ratings}>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        //precision={0.5}
+        onChange={(event, newValue) => {
+  
+          setValue(newValue);
+          if (!window.ethereum || !window.ethereum.isMetaMask) {
+            alert("Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!");
+          }
+          try{
+            yoyo(newValue)
+          }catch(e){
+            alert("error");
+          }
+          console.log(newValue) 
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+      />
+      {value !== null && <Box ml={5}>{labels[hover !== -1 ? hover : value]}</Box>}
+    </div>
         </div>
       </header>
     </div>
---
+++ src/config.js
@@ -0,0 +1,76 @@
+export const CADBURY_ADDRESS = '0x8B2090fAd5339baA659b004525E1440308159193'
+
+export const CADBURY_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "rate",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "GetRatings",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalPeople",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+
+
+
+/// smc on kovan
+
+// pragma solidity >=0.4.22 <0.7.0;
+
+// /**
+//  * @title Ratings
+//  * To store and get ratings for Cadbury
+//  */
+// contract CadburyRatings {
+
+//     uint256 totalRatings = 5;
+//     uint256 totalApplication = 1;
+
+
+//     function rate(uint256 num) public {
+//         require(num >= 0 && num <= 5 );
+//         totalRatings = totalRatings + num ;
+//         totalApplication = totalApplication + 1 ;
+//     }
+
+
+//     function GetRatings() public view returns (uint256){
+//         return ( totalRatings * 1000 ) /totalApplication;
+//     }
+    
+//     function totalPeople() public view returns (uint256){
+//         return totalApplication;
+//     }
+// }
