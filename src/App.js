e838d9cdbd73d099033322708af3543c3f9ec0c9
susmit <susmit9370@gmail.com>
2020-09-20 13:30:29 +0530

    integrates ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-// import Hls from 'hls.js'
+import ReactGA from 'react-ga';
 import { Route, Switch, Redirect } from 'react-router-dom'
 import MeetC from './containers/MeetC'
 import HomeC from './containers/HomeC'
@@ -8,6 +8,8 @@ import Landing from './components/Landing'
 import NotAvailable from './components/NotAvailable'
 
 function App() {
+  ReactGA.initialize('UA-177909748-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <Switch>
       <Route exact path="/home" component={HomeC} />
